9462991ff15a0cb68ab34862e8bea18a043f7d5a
Hem-Energy <hemenergyllc@gmail.com>
2026-08-18 12:45:10 +0400

    add mobile-first team section directly below hero on about page matching design system

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
 import AboutHero from '../components/AboutHero'
+import Team from '../components/Team'
 import Timeline from '../components/Timeline'
 import Reasons from '../components/Reasons'
 import Reviews from '../components/Reviews'
@@ -8,6 +9,7 @@ const About = () => {
   return (
     <>
       <AboutHero />
+      <Team />
       <Timeline />
       <Reasons />
       <Reviews />
